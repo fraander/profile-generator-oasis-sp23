3a07f4e0fab2c12f4033b63e7e569520d2d5b844
fraander <57777918+fraander@users.noreply.github.com>
2022-12-24 19:08:45 -0800

    setup card styling

--- script.js
+++ script.js
@@ -1,20 +1,25 @@
 function updatePage() {
-  let card = 
-    "<div class='group-box'><ul>\n" +
-      addElement("", "form-photo") +
-      addElement("", "form-name") +
-      addElement("", "form-title") +
-      addElement("School: ", "form-school") +
-      addElement("Email: ", "form-email") +
-      addElement("Phone: ", "form-phone") +
-      addElement("Fun Fact! ", "form-fun-fact") +
-      "</ul></div>";
+//   let card = 
+//     "<div class='group-box'><ul>\n" +
+//       addElement("", "form-photo") +
+//       addElement("", "form-name") +
+//       addElement("", "form-title") +
+//       addElement("School: ", "form-school") +
+//       addElement("Email: ", "form-email") +
+//       addElement("Phone: ", "form-phone") +
+//       addElement("Fun Fact! ", "form-fun-fact") +
+//       "</ul></div>";
 
-  document.body.innerHTML += card;
-  console.log(card);
+//   document.body.innerHTML += card;
+//   console.log(card);
 }
 
-function addElement(label, id) {
-   let content = document.getElementById(id).value
-   return content.length == 0 ? "" : "\t<li>" + label + content + "</li>\n";
-}
+// function addElement(label, id) {
+//    let content = document.getElementById(id).value
+//    return content.length == 0 ? "" : "\t<li>" + label + content + "</li>\n";
+// }
+
+// var loadFile = function(event) {
+// 	var image = document.getElementById("");
+// 	image.src = URL.createObjectURL(document.getElementById().target.files[0]);
+// };
