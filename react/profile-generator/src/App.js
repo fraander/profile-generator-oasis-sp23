85061bb62808b68d9529bcb4014752ff4271aba1
fraander <57777918+fraander@users.noreply.github.com>
2022-12-29 19:26:27 -0800

    styling!

--- react/profile-generator/src/App.js
+++ react/profile-generator/src/App.js
@@ -2,6 +2,7 @@ import "./App.css";
 import { useState } from "react";
 
 export function App() {
+  const [photo, setPhoto] = useState(null);
   const [name, setName] = useState("");
   const [title, setTitle] = useState("");
   const [email, setEmail] = useState("");
@@ -17,14 +18,47 @@ export function App() {
     setShowOutput(true);
   }
 
+  function onInputMade(event) {
+    event.preventDefault();
+    setShowOutput(false);
+  }
+
+  // use a styles object instead of CSS
+  const appBorder = {
+    margin: "0px",
+    cornerRadius: "0px",
+  };
+
   return (
-    <div class="app">
+    <div className="app" style={{ appBorder }}>
       <h1>Profile Generator</h1>
 
-      <div class="input">
+      <div className="input">
         <form onSubmit={handleSubmit}>
+          <label htmlFor="form-photo">Photo: </label>
+          <input
+            id="form-photo"
+            type="file"
+            // no `value` because value is a path, but we want to store a photo
+            onChange={(event) => {
+              setPhoto(URL.createObjectURL(event.target.files[0]));
+              setShowOutput(false);
+            }}
+            required
+          />
+          <br />
+
           <label htmlFor="form-name">Name: </label>
-          <input id="form-name" type="text" value={name} onChange={setName} />
+          <input
+            id="form-name"
+            type="text"
+            value={name}
+            onChange={(e) => {
+              setName(e.target.value);
+              setShowOutput(false);
+            }}
+            required
+          />
           <br />
 
           <label htmlFor="form-title">Title: </label>
@@ -32,7 +66,11 @@ export function App() {
             id="form-title"
             type="text"
             value={title}
-            onChange={setTitle}
+            onChange={(e) => {
+              setTitle(e.target.value);
+              setShowOutput(false);
+            }}
+            required
           />
           <br />
 
@@ -41,12 +79,25 @@ export function App() {
             id="form-email"
             type="email"
             value={email}
-            onChange={setEmail}
+            onChange={(e) => {
+              setEmail(e.target.value);
+              setShowOutput(false);
+            }}
+            required
           />
           <br />
 
           <label htmlFor="form-phone">Phone: </label>
-          <input id="form-phone" type="tel" value={phone} onChange={setPhone} />
+          <input
+            id="form-phone"
+            type="tel"
+            value={phone}
+            onChange={(e) => {
+              setPhone(e.target.value);
+              setShowOutput(false);
+            }}
+            required
+          />
           <br />
 
           <label htmlFor="form-fun-fact">Fun Fact: </label>
@@ -54,7 +105,11 @@ export function App() {
             id="form-fun-fact"
             type="text"
             value={fact}
-            onChange={setFact}
+            onChange={(e) => {
+              setFact(e.target.value);
+              setShowOutput(false);
+            }}
+            required
           />
           <br />
 
@@ -62,19 +117,29 @@ export function App() {
         </form>
       </div>
 
-      {showOutput && (
-        <div class="output">
-          <div class="left-section">
-            <h1>{name}</h1>
-            <p>{title}</p>
+      {showOutput ? (
+        <div className="output">
+          <div className="column">
+            <img src={photo ?? ""} alt="profile photo" />
+
+            <h1>{name ?? "error"}</h1>
+            <p>{title ?? "error"}</p>
           </div>
 
-          <div class="right-section">
-            <p>Email: {email}</p>
-            <p>Phone: {phone}</p>
-            <p>Fun Fact: {fact}</p>
+          <div className="column">
+            <p>
+              <b>Email:</b> {email ?? "error"}
+            </p>
+            <p>
+              <b>Phone:</b> {phone ?? "error"}
+            </p>
+            <p>
+              <b>Fun Fact:</b> {fact ?? "error"}
+            </p>
           </div>
         </div>
+      ) : (
+        <></>
       )}
     </div>
   );
